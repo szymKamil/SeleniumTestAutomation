7ffd611ea5b48cce97cfd22f28c9f28cf51da803
Kamil Szymański <87661417+szymKamil@users.noreply.github.com>
2025-12-07 19:43:36 +0100

    Prace nad BiDi

---
+++ src/main/sandbox/BiDi/BiDiLogs.java
@@ -0,0 +1,91 @@
+package BiDi;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.bidi.log.ConsoleLogEntry;
+import org.openqa.selenium.bidi.log.JavascriptLogEntry;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class BiDiLogs {
+
+	FirefoxDriver driver;
+
+	@BeforeMethod
+	public void setup() {
+		FirefoxOptions options = new FirefoxOptions();
+		options.setCapability("webSocketUrl", true);
+		driver = new FirefoxDriver(options);
+	}
+
+	@Test
+	void canAddConsoleMessageHandler()
+			throws ExecutionException, InterruptedException, TimeoutException, java.util.concurrent.TimeoutException {
+		CompletableFuture<ConsoleLogEntry> future = new CompletableFuture<>();
+
+		long id = ((RemoteWebDriver) driver).script().addConsoleMessageHandler(future::complete);
+
+		driver.get("https://www.selenium.dev/selenium/web/bidi/logEntryAdded.html");
+		driver.findElement(By.id("consoleLog")).click();
+
+		ConsoleLogEntry logEntry = future.get(5, TimeUnit.SECONDS);
+
+		Assert.assertEquals("Hello, world!", logEntry.getText());
+
+		((RemoteWebDriver) driver).script().removeConsoleMessageHandler(id);
+	}
+
+	@Test
+	void canRemoveConsoleMessageHandler() {
+		CopyOnWriteArrayList<ConsoleLogEntry> logs = new CopyOnWriteArrayList<>();
+
+		driver.get("https://www.selenium.dev/selenium/web/bidi/logEntryAdded.html");
+
+		long id = ((RemoteWebDriver) driver).script().addConsoleMessageHandler(logs::add);
+		((RemoteWebDriver) driver).script().removeConsoleMessageHandler(id);
+
+		driver.findElement(By.id("consoleLog")).click();
+
+		Assert.assertEquals(0, logs.size());
+	}
+
+	@Test
+	void canAddJsErrorHandler() throws ExecutionException, InterruptedException, TimeoutException, java.util.concurrent.TimeoutException {
+		CompletableFuture<JavascriptLogEntry> future = new CompletableFuture<>();
+
+		long id = ((RemoteWebDriver) driver).script().addJavaScriptErrorHandler(future::complete);
+
+		driver.get("https://www.selenium.dev/selenium/web/bidi/logEntryAdded.html");
+		driver.findElement(By.id("jsException")).click();
+
+		JavascriptLogEntry logEntry = future.get(5, TimeUnit.SECONDS);
+
+		Assert.assertEquals("Error: Not working", logEntry.getText());
+		System.out.println(logEntry.getText());
+
+		((RemoteWebDriver) driver).script().removeJavaScriptErrorHandler(id);
+	}
+
+	@Test
+	void canRemoveJsErrorHandler() {
+		CopyOnWriteArrayList<JavascriptLogEntry> logs = new CopyOnWriteArrayList<>();
+
+		long id = ((RemoteWebDriver) driver).script().addJavaScriptErrorHandler(logs::add);
+		((RemoteWebDriver) driver).script().removeJavaScriptErrorHandler(id);
+
+		driver.get("https://www.selenium.dev/selenium/web/bidi/logEntryAdded.html");
+		driver.findElement(By.id("jsException")).click();
+
+		Assert.assertEquals(0, logs.size());
+	}
+}
+
